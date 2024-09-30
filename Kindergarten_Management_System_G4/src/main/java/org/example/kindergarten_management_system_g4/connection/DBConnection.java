f6e3f10c947c45aca2e806ea871c592a6504959d
longnhhe160140@fpt.edu.vn <longnhhe160140@fpt.edu.vn>
2024-09-30 10:49:03 +0700

    update login

--- Kindergarten_Management_System_G4/src/main/java/org/example/kindergarten_management_system_g4/connection/DBConnection.java
+++ Kindergarten_Management_System_G4/src/main/java/org/example/kindergarten_management_system_g4/connection/DBConnection.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/test";
+    private static final String URL = "jdbc:mysql://localhost:3306/KindergartenManagementSystem";
     private static final String USER = "root";
-    private static final String PASSWORD = "123456";
+    private static final String PASSWORD = "long@1605";
 
     public static Connection getConnection() throws SQLException {
         try {
