f914ddc657b4fcec6dca4a21a6e6fb54da82bd60
HUYGIAVU <huyvghe163358@fpt.edu.vn>
2024-09-30 11:43:34 +0700

    Update DBConnection.java

--- Kindergarten_Management_System_G4/src/main/java/org/example/kindergarten_management_system_g4/connection/DBConnection.java
+++ Kindergarten_Management_System_G4/src/main/java/org/example/kindergarten_management_system_g4/connection/DBConnection.java
@@ -5,9 +5,10 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/test";
+    private static final String URL = "jdbc:mysql://localhost:3306/KindergartenManagementSystem";
     private static final String USER = "root";
     private static final String PASSWORD = "123456";
+
 
     public static Connection getConnection() throws SQLException {
         try {
