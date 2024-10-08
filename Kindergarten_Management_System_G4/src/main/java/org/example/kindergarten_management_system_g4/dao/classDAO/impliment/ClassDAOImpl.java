b0ed392b48b2672a297085dce4e5b36ea8936b6b
longnhhe160140@fpt.edu.vn <longnhhe160140@fpt.edu.vn>
2024-10-08 13:55:01 +0700

    Create ClassDAOImpl.java

---
+++ Kindergarten_Management_System_G4/src/main/java/org/example/kindergarten_management_system_g4/dao/classDAO/impliment/ClassDAOImpl.java
@@ -0,0 +1,7 @@
+package org.example.kindergarten_management_system_g4.dao.classDAO.impliment;
+
+import org.example.kindergarten_management_system_g4.connection.DBConnection;
+import org.example.kindergarten_management_system_g4.dao.classDAO.IClassDAO;
+
+public class ClassDAOImpl extends DBConnection implements IClassDAO {
+}
